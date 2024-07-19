dfae7d929381dadbf1325878f6f275e87b6da4ff
dqjbblp <2824057546@qq.com>
2024-07-19 11:19:08 +0800

    test

--- app/components/LastNews/index.tsx
+++ app/components/LastNews/index.tsx
@@ -1,14 +1,15 @@
-import { ArticleApi } from "@/app/api/article-api"
+// import { ArticleApi } from "@/app/api/article-api"
 import Card from "../Card"
 
 const LastNews = async () => {
-  const res = await ArticleApi.fetchToday()
-  console.log(res.length);
+  // const res = await ArticleApi.fetchToday()
+  // console.log(res.length);
   
 
   return(
     <div>
-      <Card article={res[3]} />
+      123
+      {/* <Card article={res[3]} /> */}
     </div>
   )
 }
